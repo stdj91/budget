38397059125b37f127136ae3988a271661e563e9
unknown <stdj.1907@gmail.com>
2020-05-10 18:19:33 +0200

    New Item to Budget Controller

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
 //-------------------------BUDGET CONTROLLER-----------------------------
 
 var budgetController = (function()  {
-
+   
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
     //some code
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
 
+        var data = {
+            allItems: {
+                exp: [],
+                inc: []
+            },
+            totals: {
+                exp: 0,
+                inc: 0
+            }
+        }
+
+        return {
+            addItem: function (type, des, val) {
+               
+               var newItem, ID;
+
+               // Create new ID
+
+               if (data.allItems[type].length > 0) {
+                    ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+               } else {
+                   ID = 0;
+               }
+               
+
+                // Create new item based on 'exp' or 'inc' type
+               if (type === 'exp') {
+                newItem  = new Expense(ID, des, val);
+               } else if (type === 'inc') {
+                newItem = new Income (ID, des, val)
+               }
+               // Push it intro our data structure
+                data.allItems[type].push(newItem);
+
+                //return the new element
+                return newItem;
+                
+            },  
+                    testing: function () {
+                        console.log(data)
+                    }
+        }
 
 })();
 //-------------------------UI CONTROLLER-----------------------------
@@ -38,32 +90,45 @@ var UIController = (function () {
 //--------------------- GLOBAL APP CONTROLLER-------------------------
 var controller = (function(budgetCtrl, UICtrl){
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListener = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
+
+        document.addEventListener('keypress', function(event){
+             if (event.keyCode === 13 || event.which === 13) {
+                 ctrlAddItem();
+                }
+        })
+
+    }
+
+    
 
     var ctrlAddItem = function(){
-
+        var input, newItem;
 
         //1. Get the field input data
-        var input = UICtrl.getInput();
-        console.log(input)
+        input = UICtrl.getInput();
+       
        //2. add item to the budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value)
        //3 Add the item to the UI
 
        //4. Calculate the budget
 
        //5. Display the budget on the UI
       
-    }
+    };
 
-   document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
-      
+    return {
+        init: function () {
+            console.log("app startetd");
+            setupEventListener();
+        }
+    }
    
 
-   document.addEventListener('keypress', function(event){
-       if (event.keyCode === 13 || event.which === 13) {
-           ctrlAddItem();
-       }
-   })
-
 })(budgetController, UIController);
+
+controller.init();
